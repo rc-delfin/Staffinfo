255d6a466027b384df82cb82ddc5eb4758b9369c
rcdelfin <irri.org>
2022-11-15 17:03:19 +0800

    parameters passed across routes after login invoked

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ import os
 import oauthlib
 import json
 
-
 from flask import (
     Flask,
     render_template,
     request, redirect, url_for, session, current_app
 )
-# from flask_dance.consumer import requests
 
 from flask_dance.contrib.google import make_google_blueprint, google
-
-# oauth
 from oauthlib.oauth2.rfc6749.errors import InvalidClientIdError, TokenExpiredError
-# os.environ
 from dotenv import load_dotenv
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -33,31 +28,6 @@ def _empty_session():
     session.clear()
 
 
-def getHRinfo(email, ocs_url, ocs_hdr_key):
-    def dd(hh):
-        return str(float(hh) / 8.0)
-
-    url = ocs_url
-    payload = {"email": email}
-    headers = {"X-API-KEY": ocs_hdr_key}
-
-    r = requests.get(url, params=payload, headers=headers)
-    lst = r.text.split("\t")
-
-    d = {}
-
-    if lst:
-        leaves = lst[1:]
-        for num, name in enumerate(
-                leaves
-        ):  # name is an unused variable but app crashes if removed
-            if num % 2 == 0:
-                d[leaves[num]] = [leaves[num + 1], dd(leaves[num + 1])]
-            else:
-                continue
-    return [lst[0], d]
-
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "quizbeeapp123456789"
 
@@ -70,7 +40,7 @@ google_bp = make_google_blueprint(
         "openid",
         "https://www.googleapis.com/auth/userinfo.profile",
     ],
-    redirect_to="landing_page",
+    redirect_to="login",
 )
 
 app.register_blueprint(google_bp, url_prefix="/login")
@@ -80,7 +50,7 @@ app.register_blueprint(google_bp, url_prefix="/login")
 @app.errorhandler(oauthlib.oauth2.rfc6749.errors.InvalidClientIdError)
 def token_expired(_):
     _empty_session()
-    return redirect(url_for("landing_page"))
+    return redirect(url_for("login"))
 
 
 # TOKEN ERROR HANDLING #
@@ -103,63 +73,51 @@ def template_not_found(self):
 
 
 @app.route('/', methods=["GET", "POST"])
-def landing_page():
+def login():
     if not google.authorized:
         return redirect(url_for("google.login"))
+    return redirect(url_for("staffinfo", resno=session["resno"]))
+
+
+@app.route('/staffinfo', methods=["GET", "POST"])
+def staffinfo():
+    args = request.args
+    session["resno"] = args.get("resno")
+
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+
+    payload = {}
+    api_key = os.getenv("XAPI_SHH")
+    base_uri = os.getenv("BASE_URI")
+    base_uri_get_pic = base_uri + "getpic?resno=" + session["resno"]
+    base_uri_get_info = base_uri + "getinfo?resno=" + session["resno"]
+    headers = {"X-API-KEY": api_key}
+    response_pic = requests.request("GET", base_uri_get_pic, headers=headers, data=payload)
+    response_info = requests.request("GET", base_uri_get_info, headers=headers, data=payload)
+    staff_info = json.loads(response_info.text)
+
+    resno = staff_info["resno"]
+    name = staff_info["name"]
+    label = staff_info["label"]
+    label_value = staff_info["label_value"]
+    resource_type = staff_info["resource_type"]
+    start_date = staff_info["start_date"]
+    profile = staff_info["profile"]
+    # session["picture"] = "data:image/png;base64," + r.text
+    picture = "data:image/png;base64," + response_pic.text
 
     return render_template(
-            'index.html',
-        )
-
-
-@app.route('/staffinfo/<resno>', methods=["GET", "POST"])
-def staffinfo(resno):
-    if not google.authorized:
-        return redirect(url_for("google.login"))
-
-    if resno != "":
-        resp = google.get("/oauth2/v2/userinfo")
-        assert resp.ok, resp.text
-
-        payload = {}
-        api_key = os.getenv("XAPI_SHH")
-        base_uri = os.getenv("BASE_URI")
-        base_uri_get_pic = base_uri + "getpic?resno=" + resno
-        base_uri_get_info = base_uri + "getinfo?resno=" + resno
-        headers = {"X-API-KEY": api_key}
-        response_pic = requests.request("GET", base_uri_get_pic, headers=headers, data=payload)
-        response_info = requests.request("GET", base_uri_get_info, headers=headers, data=payload)
-        print(json.loads(response_info.text))
-        staff_info=json.loads(response_info.text)
-
-        resno = staff_info["resno"]
-        name = staff_info["name"]
-        label = staff_info["label"]
-        label_value = staff_info["label_value"]
-        resource_type = staff_info["resource_type"]
-        start_date = staff_info["start_date"]
-        profile = staff_info["profile"]
-        # session["picture"] = "data:image/png;base64," + r.text
-        picture = "data:image/png;base64," + response_pic.text
-
-        return render_template(
-            'staffinfo.html',
-            resno=resno,
-            name=name,
-            label=label,
-            label_value=label_value,
-            resource_type=resource_type,
-            start_date=start_date,
-            profile=profile,
-            picture=picture,
-        )
-
-    else:
-        return render_template(
-            'error.html',
-            error_message_title="Error",
-            error_message="An error has occurred. Please contact MIS.",
-        )
+        'staffinfo.html',
+        resno=resno,
+        name=name,
+        label=label,
+        label_value=label_value,
+        resource_type=resource_type,
+        start_date=start_date,
+        profile=profile,
+        picture=picture,
+    )
 
 
 if __name__ == '__main__':
